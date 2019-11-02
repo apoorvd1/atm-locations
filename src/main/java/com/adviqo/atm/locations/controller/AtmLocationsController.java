1b32f5471d92e4f26ec9f30f441b0097aadc75ab
apoorv <ecapoorv@gmail.com>
2019-11-02 15:56:42 +0530

    add api operations

--- src/main/java/com/adviqo/atm/locations/controller/AtmLocationsController.java
+++ src/main/java/com/adviqo/atm/locations/controller/AtmLocationsController.java
@@ -3,6 +3,8 @@ package com.adviqo.atm.locations.controller;
 import com.adviqo.atm.locations.model.Locations;
 import com.adviqo.atm.locations.service.LocationService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +31,9 @@ public class AtmLocationsController {
     }
 
     @ApiOperation(value = "Returns the list of ATM Locations")
+    @ApiResponses({@ApiResponse(code = 200, message = "Retrieve ATM Locations"),
+            @ApiResponse(code = 404, message = "server not responding"),
+            @ApiResponse(code = 500, message = "internal server error")})
     @GetMapping(path = "/v1/locations")
     @ResponseStatus(OK)
     public List<Locations> findLocation() {
